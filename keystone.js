532efa758a9d76f7e133640261b31311cc79e780
botanki <karzan@botani.nu>
2017-05-09 14:34:35 +0200

    Minor changes here and there, nothing major.

--- keystone.js
+++ keystone.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 
 // Require keystone
-var keystone = require('keystone');
+var keystone = require('keystone')
 
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,21 @@ var importRoutes = keystone.importer(__dirname);
 keystone.pre('routes', middleware.initLocals);
 keystone.pre('render', middleware.flashMessages);
 
+// Handle 404 errors
+keystone.set('404', function(req, res, next) {
+    res.notfound();
+});
+
+// Handle other errors
+keystone.set('500', function(err, req, res, next) {
+    var title, message;
+    if (err instanceof Error) {
+        message = err.message;
+        err = err.stack;
+    }
+    res.err(err, title, message);
+});
+
 // Import Route Controllers
 var routes = {
 	views: importRoutes('./views'),
